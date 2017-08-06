b8b5c5e86403b6c69f7801e23c3f75ca80d80904
“Zootopia106” <pro.zootopia106@gmail.com>
2017-08-07 05:42:46 +0900

    implement Code Splitting with React-Router v4

--- src/modules/App/routes.js
+++ src/modules/App/routes.js
@@ -4,11 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 // Import Components
 import Post from '../Post';
 import ContactUs from '../ContactUs';
+import asyncComponent from '../../components/AsyncComponent';
+
+const AsyncPost = asyncComponent(() => import('../Post'));
+const AsyncContactUs = asyncComponent(() => import('../ContactUs'));
 
 const routes = (
   <Switch>
-    <Route exact path='/' component={Post} />
-    <Route path='/contactus' component={ContactUs} />
+    <Route exact path='/' component={ AsyncPost } />
+    <Route path='/contactus' component={ AsyncContactUs } />
   </Switch>
 );
 
